858428bf25fc28f801dbfab54296b6414b2ffe80
Carla Uyemura <carla.uyemura@gmail.com>
2022-11-09 15:22:24 -0300

    mudando css

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Form from '../components/Form';
 import Card from '../components/Card';
 import Filter from '../components/Filter';
-import '../App.css';
+import './App.css';
 import sylvanas from '../img/sylvans.jpg';
 import varian from '../img/varian.jpg';
 import arthas from '../img/arthas.jpg';
