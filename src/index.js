a5cbdb078d871e6e8dd2813ecbdf42380df8de1d
Carla Uyemura <carla.uyemura@gmail.com>
2023-02-08 10:19:33 -0300

    teste de build com react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './_app';
+import App from './pages/App';
 
 ReactDOM.render(
   <React.StrictMode>
